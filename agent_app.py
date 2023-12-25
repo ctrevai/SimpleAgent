8135a23e2c364bdc4cabe9707b6ee6189a17fadf
ctrevai <ctrevai@gmail.com>
2023-12-25 22:35:04 +0800

    first commit

---
+++ agent_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import agent_lib as glib
+
+
+st.set_page_config(page_title="Agent", page_icon="🤖")
+st.title("Agent")
+
+
+prompt = st.text_input("Enter your prompt")
+
+if prompt:
+    next_step = glib.TOOL_PROMPT.format(
+        tools_string=glib.tools_string, user_input=prompt)
+
+    for i in range(5):
+        output = glib.invoke_model(next_step).strip()
+        done, next_step = glib.single_agent_step(next_step, output)
+
+        if not done:
+            st.write('Not done yet ' + output)
+        else:
+            st.write('Final answer is ' + next_step)
+            break
